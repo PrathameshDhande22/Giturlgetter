39454d0b45c22dcbe36ae4e8d08d922f5504777c
Prathamesh Dhande <prathameshdhande534@gmail.com>
2022-10-29 21:55:44 +0530

    Modified the setup.py file

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md",'r') as f:
   LNG=f.read()
 
 setup(name="Giturlgetter",
-version="0.3",
+version="0.4",
 description="Command Line to Extract the git url",
 author="Prathamesh Dhande",
 author_email='prathameshdhande534@gmail.com',
 long_description=LNG,
 long_description_content_type='text/markdown',
 packages=['command_line'],
-install_requires=['clipboard','PyGithub','requests'],
+install_requires=['clipboard','PyGithub','requests','pyfiglet','python-dotenv'],
 entry_points={
     'console_scripts': [
             'giturl=command_line.__main__:run',
@@ -20,9 +20,9 @@ entry_points={
 classifiers=[
     'Development Status :: 5 - Production/Stable',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
     'Intended Audience :: Developers',      # Define that your audience are developers   # Again, pick a license   #Specify which pyhton versions that you want to support
-    'Programming Language :: Python :: 3.4',
-    'Programming Language :: Python :: 3.5',
-    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.9',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.10',
   ]
 
 )
